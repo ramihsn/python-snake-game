2c5497036031d4e213ee60518175314a93a99dbb
Rami Hassan <hsn.rami@gmail.com>
2024-09-18 22:23:55 +0300

    make the snake start moveing

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from turtle import Screen, Turtle
+from threading import Event
+from typing import Callable
+import time
 
 
 class SnakeTile(Turtle):
@@ -6,6 +9,7 @@ class SnakeTile(Turtle):
         super().__init__(shape)
         self.width = 20
         self.color(color)
+        self.penup()
         if x and y:
             self.goto(x, y)
         elif x or y:
@@ -16,12 +20,24 @@ class SnakeTile(Turtle):
 
 
 class Snake:
-    def __init__(self) -> None:
-        self.segments = list[SnakeTile] = [
+    def __init__(self, game_event: Event, delay_between_moves: float, update_cb: Callable[[], None]) -> None:
+        self._segments: list[SnakeTile] = [
             SnakeTile(),
             SnakeTile(x=-20),
             SnakeTile(x=-40),
         ]
+        self._game_event = game_event
+        self._update_cb = update_cb
+        self._delay_between_moves = delay_between_moves
+        self._update_cb()
+
+    def start(self):
+        while not self._game_event.is_set():
+            print(end='.', flush=True)
+            for segment in self._segments:
+                segment.forward(20)
+            self._update_cb()
+            time.sleep(self._delay_between_moves)
 
 
 class Board():
@@ -30,17 +46,24 @@ class Board():
         self._screen.setup(width=600, height=600)
         self._screen.bgcolor('black')
         self._screen.title("Snake Game")
+        self._screen.tracer(0)
+        self.game_ended = Event()
+
+    def update(self):
+        self._screen.update()
 
     def __enter__(self):
         return self
 
     def __exit__(self, *args):
         self._screen.exitonclick()
+        self.game_ended.set()
 
 
 def main():
     with Board() as board:  # noqa
-        snake = Snake()  # noqa
+        snake = Snake(board.game_ended, .1, board.update)  # noqa
+        snake.start()
 
 
 if __name__ == "__main__":
